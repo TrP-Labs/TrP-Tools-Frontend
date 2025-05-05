8aef686399b3db20c4e0cb5185382d0e7ed141f7
Luna <mail@tickogrey.com>
2025-05-05 14:20:53 -0700

    changed topbar components location

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "@/app/globals.css";
 
-import Header from '@/components/Header'
+import Header from '@/components/topbar/Header'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
